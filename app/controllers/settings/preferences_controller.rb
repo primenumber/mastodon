aa737e307687c2d6cc8ce057175a762fa534bea6
primenumber <prime@kmc.gr.jp>
2023-07-07 02:18:54 +0900

    :Bigger publish button, based on https://github.com/akane-blue/mastodon/commit/8345a64338db69f50fe636efadd7d1fca64657a7

--- app/controllers/settings/preferences_controller.rb
+++ app/controllers/settings/preferences_controller.rb
@@ -55,6 +55,8 @@ class Settings::PreferencesController < Settings::BaseController
       :setting_use_pending_items,
       :setting_trends,
       :setting_crop_images,
+      :setting_bigger_publish,
+      :setting_always_send_emails,
       notification_emails: %i(follow follow_request reblog favourite mention digest report pending_account trending_tag),
       interactions: %i(must_be_follower must_be_following must_be_following_dm)
     )
--- app/lib/user_settings_decorator.rb
+++ app/lib/user_settings_decorator.rb
@@ -39,6 +39,8 @@ class UserSettingsDecorator
     user.settings['use_pending_items']   = use_pending_items_preference if change?('setting_use_pending_items')
     user.settings['trends']              = trends_preference if change?('setting_trends')
     user.settings['crop_images']         = crop_images_preference if change?('setting_crop_images')
+    user.settings['bigger_publish']      = bigger_publish_preference if change?('setting_bigger_publish')
+    user.settings['always_send_emails']  = always_send_emails_preference if change?('setting_always_send_emails')
   end
 
   def merged_notification_emails
@@ -135,6 +137,14 @@ class UserSettingsDecorator
 
   def crop_images_preference
     boolean_cast_setting 'setting_crop_images'
+  end
+
+  def bigger_publish_preference
+    boolean_cast_setting 'setting_bigger_publish'
+  end
+
+  def always_send_emails_preference
+    boolean_cast_setting 'setting_always_send_emails'
   end
 
   def boolean_cast_setting(key)
