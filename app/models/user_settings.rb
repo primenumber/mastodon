c40b658d31b1e1b12b8aa144e9adf8855529b658
primenumber <prime@kmc.gr.jp>
2023-12-24 04:35:15 +0900

    Bigger-publish button

--- app/models/user_settings.rb
+++ app/models/user_settings.rb
@@ -32,6 +32,8 @@ class UserSettings
     setting :expand_content_warnings, default: false
     setting :display_media, default: 'default', in: %w(default show_all hide_all)
     setting :auto_play, default: false
+    # poyo-server custom
+    setting :bigger_publish, default: false
   end
 
   namespace :notification_emails do
